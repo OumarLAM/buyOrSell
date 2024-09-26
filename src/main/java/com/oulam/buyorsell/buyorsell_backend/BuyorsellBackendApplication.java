d4c11a9d9db5a2e77a5a3bf26b0c17740c56e709
OumarLAM <oumarlam154@gmail.com>
2024-09-26 17:27:39 +0000

    Add project initialization and dependencies

---
+++ src/main/java/com/oulam/buyorsell/buyorsell_backend/BuyorsellBackendApplication.java
@@ -0,0 +1,13 @@
+package com.oulam.buyorsell.buyorsell_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyorsellBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyorsellBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oulam/buyorsell/buyorsell_backend/BuyorsellBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oulam.buyorsell.buyorsell_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyorsellBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
